147b5d1c5eb5c8c2ae3766dd2e861f4c24ebc156
Kenny-AngJY <fire_light2010@hotmail.com>
2024-07-03 22:38:59 +0800

    Upgrade terraform required_version & aws provider version.

--- Terraform/provider.tf
+++ Terraform/provider.tf
@@ -2,9 +2,9 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">=5.40.0"
+      version = ">=5.50.0"
     }
   }
 
-  required_version = "~> 1.7"
+  required_version = "~> 1.8"
 }
